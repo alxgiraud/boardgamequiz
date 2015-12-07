1395f9abb00fecf0f6bfcbdbcd47d8d02088c5b9
axlgiraud <alexandre.giraud89@gmail.com>
2015-12-07 10:42:22 +0100

    Code refactoring. Add round counter.

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,66 +1,81 @@
-/*global define*/
+/*global define, angular*/
 define(['app', 'services/quizServices'], function (app) {
     'use strict';
-    app.controller('HomeCtrl', ['$scope', '$interval', 'quizServices', function ($scope, $interval, quizServices) {
+    app.controller('HomeCtrl', ['$scope', '$interval', '$timeout', 'quizServices', function ($scope, $interval, $timeout, quizServices) {
 
         var quizNumber = 0,
             countdown,
-            stopCountdown = function () {
-                if (angular.isDefined(countdown)) {
-                    $interval.cancel(countdown);
-                    countdown = undefined;
-                }
-            },
-            endQuestion = function (win) {
-                stopCountdown();
+            quizzHelper = {
+                getGames: function () {
+                    quizNumber += 1;
+                    $scope.quizNumber = quizNumber;
+                    quizServices.getGames().then(function (result) {
+                        var games = result.data;
 
-                if (win) {
-                    console.log("WIN");
-                } else {
-                    console.log("LOOSE");
-                }
+                        $scope.winningGame = games[Math.floor(Math.random() * games.length)];
+                        $scope.choices = quizServices.convertGamesToChoices(games);
+                        $scope.classChoice = null;
 
-                getGames();
-            },
-            decreaseCountdown = function () {
-                if ($scope.counter > 0) {
-                    $scope.counter -= 1;
-                } else {
-                    endQuestion(0);
-                }
-            },
-            getGames = function () {
-                quizNumber += 1;
-                if (quizNumber >= 10) {
-                    //TODO: End of the round!
-                }
-                $scope.quizNumber = quizNumber;
-                quizServices.getGames().then(function (result) {
-                    var games = result.data;
+                        $scope.counter = 150;
+                        countdown = $interval(quizzHelper.decreaseCountdown, 100);
 
-                    $scope.winningGame = games[Math.floor(Math.random() * games.length)];
-                    $scope.choices = quizServices.convertGamesToChoices(games);
-                    
-                    $scope.counter = 150;
-                    countdown = $interval(decreaseCountdown, 100);
+                    }, function (error) {
+                        $scope.error = 'Oups! An error occurred while retrieving the games.';
+                    });
+                },
+                decreaseCountdown: function () {
+                    if ($scope.counter > 0) {
+                        $scope.counter -= 1;
+                    } else {
+                        quizzHelper.endQuestion(0);
+                    }
+                },
+                endQuestion: function (win) {
+                    quizzHelper.stopCountdown();
+                    $scope.classChoice = 'choice-disabled';
 
-                }, function (error) {
-                    $scope.error = 'Oups! An error occurred while retrieving the games.';
-                });
+                    if (win) {
+                        console.log("WIN");
+                    } else {
+                        console.log("LOOSE");
+                    }
+
+                    $timeout(function () {
+                        if (quizNumber < 10) {
+                            quizzHelper.getGames();
+                        } else {
+                            console.log("GAME OVER");
+                        }
+                    }, 1000);
+                },
+                stopCountdown: function () {
+                    if (angular.isDefined(countdown)) {
+                        $interval.cancel(countdown);
+                        countdown = undefined;
+                    }
+                },
+                handleClickOnChoice: function (id) {
+                    if ($scope.classChoice !== 'choice-disabled') {
+                        quizzHelper.stopCountdown();
+                        if (id === $scope.winningGame.game_id) {
+                            $scope.score += 10 + Math.ceil($scope.counter / 10);
+
+                            quizzHelper.endQuestion(1);
+
+                        } else {
+                            quizzHelper.endQuestion(0);
+                        }
+                    }
+                }
             };
 
         $scope.score = 0;
         $scope.quizNumber = 1;
-        getGames();
+
+        quizzHelper.getGames();
 
         $scope.selectGame = function (id) {
-            stopCountdown();
-            if (id === $scope.winningGame.game_id) {
-                $scope.score += 30 - Math.ceil($scope.counter / 10);
-                endQuestion(1);
-            } else {
-                endQuestion(0);
-            }
+            quizzHelper.handleClickOnChoice(id);
         };
 
     }]);
