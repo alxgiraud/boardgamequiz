3bbaa5252ecf49ec79c5180e1baadaba535f0407
axlgiraud <alexandre.giraud89@gmail.com>
2016-01-14 17:35:33 +0100

    Fix IE caching $http requests

--- public/js/app.js
+++ public/js/app.js
@@ -14,7 +14,19 @@ define(['angularAMD', 'angular-route', 'angular-animate', 'roundProgress', 'smoo
         MAX_COMBO_BONUS: 5              // Number of combo max
     });
 
+    app.config(['$httpProvider', function ($httpProvider) {
 
+        if (!$httpProvider.defaults.headers.get) {
+            $httpProvider.defaults.headers.get = {};
+        }
+
+        //disable IE ajax request caching
+        $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+        
+        $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+        $httpProvider.defaults.headers.get.Pragma = 'no-cache';
+    }]);
+    
     app.config(function ($routeProvider) {
         $routeProvider
             .when("/", angularAMD.route({
